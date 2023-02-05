905b568ac0b39d8ef879a1c9def2c96bbd88fcb8
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,14 +6,13 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:17:22 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/06/08 08:25:09 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 11:40:31 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//compare byte string
-
+// Compare byte string
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	if (!n)
