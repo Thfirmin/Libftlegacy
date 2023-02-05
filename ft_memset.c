78d85a8369fb0a70ea4274fa991a401bac28163f
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,22 +6,19 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:22:24 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/06/14 21:11:56 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 11:15:28 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//write a byte to n byte string
-
+// Write a byte to n byte string
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*temp;
 
 	temp = b;
 	while (len--)
-	{
 		*temp++ = c;
-	}
 	return (b);
 }
