cce65d763b70c743f090b8c33fa0e7afe8a3a00d
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,24 +6,23 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:36:45 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/06/22 16:28:01 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 11:41:39 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//locate a substring in a n lenght string
-
+// Locate a substring in a n lenght string
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	index;
 
 	index = 0;
-	if (*needle == '\0')
+	if (!*needle)
 		return ((char *) haystack);
-	while ((index <= len) && *haystack != '\0')
+	while ((index <= len) && *haystack)
 	{
-		if (*(needle + index) == '\0')
+		if (!*(needle + index))
 			return ((char *)haystack);
 		else if (*(haystack + index) == *(needle + index))
 			index ++;
