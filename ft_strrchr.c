f80cc4f99b2082be0b4163ca2a7ad7eeab7cb966
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,31 +6,20 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:41:57 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/05/27 05:11:55 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 12:15:56 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//locate last occurrence of character in string
-
+// Locate last occurrence of character in string
 char	*ft_strrchr(const char *s, int c)
 {
 	int	count;
-	int	lastocur;
 
-	count = 0;
-	lastocur = -1;
-	while (s[count] != '\0')
-	{
-		if (s[count] == (char) c)
-			lastocur = count;
-		count ++;
-	}
-	if (lastocur >= 0)
-		return ((char *) s + lastocur);
-	else if (c == 0)
-		return ((char *) s + count);
-	else
-		return (0);
+	count = (ft_strlen(s) + 1);
+	while (--count > -1)
+		if (*(s + count) == (char) c)
+			return ((char *)s + count);
+	return (0);
 }
