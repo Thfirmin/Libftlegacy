d2f105fcfacb107cdb03e8b8aa32bfb38c090cea
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 17:21:17 -0300

    Add isascii

--- Makefile
+++ Makefile
@@ -18,7 +18,8 @@ NAME	= libft.a
 
 SRCS	= ft_isalpha.c \
 		  ft_isdigit.c \
-		  ft_isalnum.c
+		  ft_isalnum.c \
+		  ft_isascii.c
 
 OBJS	= $(SRCS:.c=.o)
 # <+-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-' #
--- ft_isascii.c
+++ ft_isascii.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thfirmin <thfirmin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/20 19:50:29 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/01/20 23:42:15 by thfirmin         ###   ########.fr       */
+/*   Created: 2022/05/12 20:14:22 by thfirmin          #+#    #+#             */
+/*   Updated: 2022/05/15 10:24:14 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <unistd.h>
-# include <stdlib.h>
+#include "libft.h"
 
-// is
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
+//test for ASCII character between 0 and octal 0177
 
-#endif
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+		return (1);
+	else
+		return (0);
+}
