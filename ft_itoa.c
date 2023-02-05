4384686baf613e8578932d0e4f56d092d741b718
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thiagofirmino2001@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 15:16:31 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/06/22 17:11:59 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 12:17:02 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 // Turn a integer number in a numeric string
 
-static void	ft_nbrlen(int n, int *len);
+static int	ft_nbrlen(int n);
 
 char	*ft_itoa(int n)
 {
@@ -22,13 +22,12 @@ char	*ft_itoa(int n)
 	char			*str;
 	unsigned int	aux;
 
-	len = 0;
-	ft_nbrlen(n, &len);
+	len = ft_nbrlen(n);
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (0);
 	if (n < 0)
-		aux = (n * -1);
+		aux = -n;
 	else
 		aux = n;
 	*(str + len) = '\0';
@@ -42,22 +41,18 @@ char	*ft_itoa(int n)
 	return (str);
 }
 
-static void	ft_nbrlen(int n, int *len)
+static int	ft_nbrlen(int n)
 {
 	unsigned int	aux;
+	int				len;
 
-	if (n < 0)
-	{
-		*len += 1;
-		aux = (n * -1);
-	}
+	len = 0;
+	if ((n < 0) && ++len)
+		aux = -n;
 	else
 		aux = n;
 	if (aux >= 10)
-	{
-		*len += 1;
-		ft_nbrlen((aux / 10), len);
-	}
-	if (aux < 10)
-		*len += 1;
+		len += ft_nbrlen((aux / 10));
+	len ++;
+	return (len);
 }
