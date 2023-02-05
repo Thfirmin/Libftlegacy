d46f17f1dd97ede7c26397b448c9bc3e2c56dadd
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,14 +6,13 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:15:37 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/06/22 17:22:28 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/20 20:49:33 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//locate byte in byte string
-
+// Locate byte in byte string
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	while (n --)
