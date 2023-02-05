5eca938d9465a801e7ee2e71f0bce839ce7c41d5
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,26 +6,22 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:25:02 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/05/27 05:10:59 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 11:22:11 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//locate first occurrence of character in string
-
+// Locate first occurrence of character in string
 char	*ft_strchr(const char *s, int c)
 {
 	int	count;
 
-	count = 0;
-	while (s[count] != '\0')
-	{
-		if (s[count] == (char) c)
+	count = -1;
+	while (*(s + ++count))
+		if (*(s + count) == (char) c)
 			return ((char *) s + count);
-		count ++;
-	}
-	if (c == 0)
+	if (!c)
 		return ((char *) s + count);
 	else
 		return (0);
