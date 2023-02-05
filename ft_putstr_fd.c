c4e4bfa8a5f7436ffa35f6457067633cdf40ea94
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,16 +6,15 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 05:41:39 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/06/15 06:01:38 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 11:17:24 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// Output a string in the gived file descriptor
 void	ft_putstr_fd(char *s, int fd)
 {
 	while (*s)
-	{
 		write(fd, s++, 1);
-	}
 }
