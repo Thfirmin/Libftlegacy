45f3e03f1c952e056ab88c832a0a0f5e2d30f104
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 17:29:02 -0300

    Add strlcpy

--- Makefile
+++ Makefile
@@ -28,7 +28,8 @@ SRCS	= ft_isalpha.c \
 		  ft_strlen.c \
 		  ft_strchr.c \
 		  ft_strrchr.c \
-		  ft_strncmp.c
+		  ft_strncmp.c \
+		  ft_strlcpy.c
 
 OBJS	= $(SRCS:.c=.o)
 # <+-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-' #
--- libft.h
+++ libft.h
@@ -33,5 +33,6 @@ size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
