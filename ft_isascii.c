9f71fad3f11705a46e1d9d121e5306a3361c9a98
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,18 +6,16 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:14:22 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/05/15 10:24:14 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 10:47:49 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//test for ASCII character between 0 and octal 0177
-
+// Test for ASCII character between 0 and octal 0177
 int	ft_isascii(int c)
 {
 	if (c >= 0 && c <= 127)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
