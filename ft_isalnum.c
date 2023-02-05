3ce97ab20e4d855b0cf4cb0df753666c76645c3c
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                      :+:     :+:            */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:13:53 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/10/08 20:15:22 by Thinotsuki   ###     ###.br              */
+/*   Updated: 2023/01/21 10:47:24 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//alphanumeric character test, it's equal (isalpha || isdigit)
-
+// Alphanumeric character test, it's equal (isalpha || isdigit)
 int	ft_isalnum(int c)
 {
 	if (c >= '0' && c <= '9')
-	{
-	}
+		return (1);
 	else if (c >= 'a' && c <= 'z')
-	{
-	}
+		return (1);
 	else if (c >= 'A' && c <= 'Z')
-	{
-	}
+		return (1);
 	else
 		return (0);
-	return (1);
 }
