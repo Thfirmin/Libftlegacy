11b55e7e309efa56681f5613809e4786e76e4dfc
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,20 +6,20 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:35:17 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/05/15 10:05:21 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 11:33:10 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//find length of string
-
+// Find length of string
 size_t	ft_strlen(const char *str)
 {
 	unsigned int	count;
 
 	count = 0;
-	while (str[count] != '\0')
-		count ++;
+	if (str)
+		while (*(str + count))
+			count ++;
 	return (count);
 }
