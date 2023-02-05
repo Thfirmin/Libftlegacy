9cf65c72a2063b96bac566cdab3062703f5019df
Thfirmin <thfirmin.dsantos@gmail.com>
2023-02-05 18:34:36 -0300

    Otimizing strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,20 +6,19 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 20:26:01 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/06/08 01:09:44 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/21 11:31:14 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//size-bounded string concatenation
-
+// Size-bounded string concatenation
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	t_size;
 
 	t_size = ft_strlen(src);
-	while (*dst != '\0' && dstsize > 0)
+	while (*dst && dstsize > 0)
 	{
 		t_size ++;
 		dst ++;
@@ -27,7 +26,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	}
 	while (dstsize >= 1)
 	{
-		if (dstsize == 1 || *src == '\0')
+		if (dstsize == 1 || !*src)
 		{
 			*dst = '\0';
 			break ;
